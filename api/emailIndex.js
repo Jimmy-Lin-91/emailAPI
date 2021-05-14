c159f01e291dfd324baeed1d43dd759905ced7bb
Jimmy Lin <jimmy.lin0402@gmail.com>
2021-05-14 13:16:33 -0700

    Backend API for heroku to receive emails.

---
+++ api/emailIndex.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const apiKey = require('../config.json');
+const app = express();
+const cors = require('cors');
+const sendGrid = require('@sendgrid/mail');
+const port = process.env.port || 3030;
+app.use(bodyParser.json());
+app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+app.get('/api', (req, res, next) => {
+  res.send('Api is running!');
+});
+app.post('/api/email', (req, res, next) => {
+  sendGrid.setApiKey(apiKey.key);
+  const msg = {
+    to: 'jimmy.lin0402@gmail.com',
+    from: 'jimmy.lin0402@gmail.com',
+    subject: req.body.subject,
+    text: `${req.body.name} says ${req.body.message}  You can contact them at ${req.body.email}`
+  }
+  sendGrid.send(msg)
+  .then((result) => {
+    res.status(200).json({
+      success: true
+    })
+  })
+  .catch((err) => {
+    console.log(err.response.body);
+    })
+});
+
+app.listen(`${port}`, () => {
+  console.log(`Listening from ${port}`);
+});
